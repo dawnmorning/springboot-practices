cc1dd0187eb91b4f2c676174fbf482b050d6d62c
김종혁 <notify9637@naver.com>
2023-10-10 15:03:31 +0900

    spring boot test

---
+++ springboot-myapplication/src/test/java/ex03/MyApplicationTest.java
@@ -0,0 +1,25 @@
+package ex03;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ex03.component.MyComponent;
+
+/*
+ * Spring Test
+ * Error : SpringBoot Test Integration(SpringBootTest)에서는 
+ * @Configuration을 달고 있는 설정 클래스는 자동으로 스캔하지 못한다.
+ */
+@SpringBootTest
+public class MyApplicationTest {
+	@Autowired
+	private MyComponent myComponent;
+
+	@Test
+	public void myComPonentNotNull() {
+		assertNotNull(myComponent);
+	}
+}
